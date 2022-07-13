1f4f15b8570d361379abc6e4c9d137bfef6cc170
Amrish-Kumar-G <amrishkumarg@virtusa.com>
2022-07-13 23:13:13 +0530

    authenticated added

---
+++ src/Model/Authentication.java
@@ -0,0 +1,34 @@
+package Model;
+
+public class Authentication {
+
+private String name;
+private String password;
+private String nickname;
+private String mobilenumber;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getNickname() {
+	return nickname;
+}
+public void setNickname(String nickname) {
+	this.nickname = nickname;
+}
+public String getMobilenumber() {
+	return mobilenumber;
+}
+public void setMobilenumber(String mobilenumber) {
+	this.mobilenumber = mobilenumber;
+}
+
+}
