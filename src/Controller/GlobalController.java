8d131c805e6bff02afed78ec178af0e70ff5e0d5
Amrish-Kumar-G <amrishkumarg@virtusa.com>
2022-07-13 23:01:19 +0530

    few changes

--- src/Controller/GlobalController.java
+++ src/Controller/GlobalController.java
@@ -6,7 +6,7 @@ import Service.Globaloperation;
 public class GlobalController {
 public void SendMessage(Broad broad) {
 	Globaloperation globaloperation=new Globaloperation();
-	System.out.print(broad.getSender()+broad.getMessage());
+	//System.out.print(broad.getSender()+broad.getMessage());
 	globaloperation.SendMessage(broad);
 }
 }
